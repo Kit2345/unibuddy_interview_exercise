1fef2a4c6f5018673300fad719c868ab3e94f9ac
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-07-11 18:17:31 +0100

    plan added

---
+++ plan.js
@@ -0,0 +1,34 @@
+/* Plan for adding Tags to messages
+
+Tdd approach
+
+1) Update chatMessage variable to contains tags
+
+2) Run test to see if it fails
+Expect: to fail or get an error, seeing as tags dont exist 
+
+3) Fix tests by adding Tags to necessary places 
+
+4) Write tests for adding tags 
+- add single tag to a message
+- add multiple tags to a message
+- add duplicate tag
+
+5) Fix to pass adding tags tests
+
+6) Write tests for removing tags 
+- remove single tag from a message
+- remove multiple tags from a message 
+- attempt to remove a tag which doesn't exist
+
+7) Fix to pass removing tags test
+
+8) Write tests for editing tags
+- edit single tag in a message
+- edit multiple tags in a message
+- attempt to edit a tag to an invalid value and ensure it handles correctly 
+
+9) Fix to pass edit tag tests
+
+
+*/
